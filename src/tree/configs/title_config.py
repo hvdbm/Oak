e7034c174d2151a871989d7dfcf8aa53a2acacd1
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2025-01-13 22:09:22 -0500

    fix typo

--- src/tree/configs/title_config.py
+++ src/tree/configs/title_config.py
@@ -1,10 +1,7 @@
 from src.tree.configs.font_config import FontConfig
 
 class TitleConfig(FontConfig):
-  def __init__(self,
-    location: str = "t",
-    **kwargs
-  ):
+  def __init__(self, location: str = "t", **kwargs):
     self.location = location
 
     FontConfig.__init__(self, **kwargs)
